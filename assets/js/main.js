974783fd67de75b2d5fe3d85debd9f5884c45e6f
Lucas Santos <ceo.lucascs@gmail.com>
2021-11-12 01:55:39 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,16 +13,14 @@
                     ulQuicklinkTopMobile = document.getElementById("ul-quicklink-top-mobile");
 
                 hamburguerMenu.addEventListener("click", (() => {
-                        hamburguerMenu.classList.toggle("active"),
-                            setTimeout((() => {
-                                ulQuicklinkTopMobile.classList.add("ul-quicklink-top-mobile")
-                            }), 500)
-                    })),
-
-                    closeUlQuicklinkTopMobile.addEventListener("click", (() => {
-                        hamburguerMenu.classList.toggle("active"),
-                            ulQuicklinkTopMobile.classList.remove("ul-quicklink-top-mobile")
-                    }))
+                    hamburguerMenu.classList.toggle("active"),
+                        setTimeout((() => {
+                            ulQuicklinkTopMobile.classList.add("ul-quicklink-top-mobile")
+                        }), 500)
+                })), closeUlQuicklinkTopMobile.addEventListener("click", (() => {
+                    hamburguerMenu.classList.toggle("active"),
+                        ulQuicklinkTopMobile.classList.remove("ul-quicklink-top-mobile")
+                }))
 
             },
             // OPEN USER ACCOUNT MOBILE - HEADER //
@@ -33,15 +31,12 @@
                     ulUserAccountMobile = document.getElementById("ul-user-account-mobile");
 
                 openUlUserAccountMobile.addEventListener("click", (() => {
-                        ulUserAccountMobile.classList.add("ul-user-account-mobile")
-                    })),
-
-                    closeUlUserAccountMobile.addEventListener("click", (() => {
-                        ulUserAccountMobile.classList.remove("ul-user-account-mobile")
-                    }))
+                    ulUserAccountMobile.classList.add("ul-user-account-mobile")
+                })), closeUlUserAccountMobile.addEventListener("click", (() => {
+                    ulUserAccountMobile.classList.remove("ul-user-account-mobile")
+                }))
 
             },
-
             ////////////////////
             // INIT FUNCTIONS //
             ////////////////////
